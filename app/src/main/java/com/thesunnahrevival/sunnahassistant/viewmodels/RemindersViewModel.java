1e45772cf1b14ff4c83bb06345f2d9df9b6595b5
Said Muhammad <saidmsaid81@gmail.com>
2020-03-21 18:22:26 +0300

    Removed restrictions on modifying the Next Reminder

--- app/src/main/java/com/thesunnahrevival/sunnahassistant/viewmodels/RemindersViewModel.java
+++ app/src/main/java/com/thesunnahrevival/sunnahassistant/viewmodels/RemindersViewModel.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CompoundButton;
-import android.widget.Toast;
 
 import com.thesunnahrevival.sunnahassistant.data.model.AppSettings;
 import com.thesunnahrevival.sunnahassistant.data.model.Reminder;
@@ -98,10 +97,6 @@ public class RemindersViewModel extends SunnahAssistantViewModel implements Remi
 
     @Override
     public void openBottomSheet(View v, Reminder reminder, boolean isNextReminder) {
-        if (isNextReminder) {
-            Toast.makeText(getApplication(), "Disable the Reminder First before editing...", Toast.LENGTH_LONG).show();
-            return;
-        }
         ReminderDetailsFragment bottomSheetFragment = new ReminderDetailsFragment();
         if (v.getContext() instanceof MainActivity) {
             FragmentManager fm = ((MainActivity) v.getContext()).getSupportFragmentManager();
