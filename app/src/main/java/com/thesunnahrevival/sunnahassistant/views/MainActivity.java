b7eb34fb9de1d3f8c5b82199c6c8561a87aa2785
saidmsaid81 <saidmsaid81@gmail.com>
2020-03-25 17:54:34 +0300

    Added option to filter reminders by category

--- app/src/main/java/com/thesunnahrevival/sunnahassistant/views/MainActivity.java
+++ app/src/main/java/com/thesunnahrevival/sunnahassistant/views/MainActivity.java
@@ -11,19 +11,22 @@ import android.widget.Toast;
 
 import com.google.android.gms.oss.licenses.OssLicensesMenuActivity;
 import com.thesunnahrevival.sunnahassistant.R;
+import com.thesunnahrevival.sunnahassistant.data.model.AppSettings;
 import com.thesunnahrevival.sunnahassistant.utilities.SunnahAssistantUtil;
 import com.thesunnahrevival.sunnahassistant.viewmodels.RemindersViewModel;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.PopupMenu;
 import androidx.lifecycle.ViewModelProviders;
 
 import static androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO;
 import static androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, PopupMenu.OnMenuItemClickListener, PopupMenu.OnDismissListener {
 
     private RemindersViewModel mViewModel;
+    private String mFilteredReminderCategories = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,33 +67,66 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.settings:
                 startActivity(new Intent(this, SettingsActivity.class));
                 return true;
+            case R.id.filter:
+                filterReminderByCategory();
+                return true;
             case R.id.about:
                 AboutAppFragment fragment = new AboutAppFragment();
                 fragment.show(getSupportFragmentManager(), "about");
-                break;
+                return true;
             case R.id.feedback:
                 Intent intent = SunnahAssistantUtil.generateEmailIntent();
                 if (intent.resolveActivity(getPackageManager()) != null)
                     startActivity(intent);
                 else
                     Toast.makeText(this, getString(R.string.no_email_app_installed), Toast.LENGTH_LONG).show();
-                break;
+                return true;
             case R.id.rate_this_app:
                 SunnahAssistantUtil.openPlayStore(this, getPackageName());
-                break;
+                return true;
             case R.id.support_developer:
                 SunnahAssistantUtil.openPlayStore(this, "com.thesunnahrevival.supportdeveloper");
-                break;
+                return true;
             case R.id.more_apps:
                 SunnahAssistantUtil.openDeveloperPage(this);
-                break;
+                return true;
             case R.id.oss_licenses:
                 startActivity(new Intent(this, OssLicensesMenuActivity.class));
-                break;
+               return true;
 
         }
         return super.onOptionsItemSelected(item);
 
+    }
+
+    private void filterReminderByCategory() {
+        PopupMenu popup = new PopupMenu(MainActivity.this, findViewById(R.id.filter));
+        popup.setOnMenuItemClickListener(MainActivity.this);
+        popup.inflate(R.menu.filter_category);
+        popup.setOnDismissListener(this);
+        AppSettings settings = mViewModel.mSettings.getValue();
+
+        MenuItem displayAllMenuItem = popup.getMenu().add(
+                R.id.category_display_filter, Menu.NONE, Menu.NONE, getString(R.string.display_all))
+                .setCheckable(true);
+
+        if (mFilteredReminderCategories.matches(""))
+            displayAllMenuItem.setChecked(true);
+        if (settings != null){
+            for (String categoryTitle : settings.getCategories()){
+                if (!categoryTitle.matches("\\+ Create New Category")){
+                    MenuItem categoryItem = popup.getMenu().add(
+                            R.id.category_display_filter, Menu.NONE, Menu.NONE, categoryTitle
+                    );
+
+                    categoryItem.setCheckable(true);
+
+                    if (mFilteredReminderCategories.contains(categoryTitle))
+                        categoryItem.setChecked(true);
+                }
+            }
+        }
+        popup.show();
     }
 
     @Override
@@ -98,5 +134,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (v.getId() == R.id.fab) {
             mViewModel.openBottomSheet(v, null);
         }
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        if (!item.getTitle().toString().matches(getString(R.string.display_all)))
+            mFilteredReminderCategories = item.getTitle().toString();
+        else
+            mFilteredReminderCategories = "";
+        item.setChecked(!item.isChecked());
+        return true;
+    }
+
+    @Override
+    public void onDismiss(PopupMenu menu) {
+        mViewModel.mCategoriesToDisplay.setValue(mFilteredReminderCategories);
     }
 }
---
+++ app/src/main/java/com/thesunnahrevival/sunnahassistant/views/MainFragment.kt
@@ -0,0 +1,259 @@
+package com.thesunnahrevival.sunnahassistant.views
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.Toolbar
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.ItemTouchHelper
+import com.getkeepsafe.taptargetview.TapTarget
+import com.getkeepsafe.taptargetview.TapTargetSequence
+import com.google.android.material.snackbar.Snackbar
+import com.thesunnahrevival.sunnahassistant.R
+import com.thesunnahrevival.sunnahassistant.data.model.AppSettings
+import com.thesunnahrevival.sunnahassistant.data.model.Reminder
+import com.thesunnahrevival.sunnahassistant.databinding.ContentMainBinding
+import com.thesunnahrevival.sunnahassistant.utilities.SunnahAssistantUtil
+import com.thesunnahrevival.sunnahassistant.utilities.TimeDateUtil
+import com.thesunnahrevival.sunnahassistant.viewmodels.RemindersViewModel
+import com.thesunnahrevival.sunnahassistant.views.adapters.ReminderListAdapter
+import com.thesunnahrevival.sunnahassistant.views.interfaces.OnDeleteReminderListener
+
+class MainFragment : Fragment(), OnItemSelectedListener, OnDeleteReminderListener {
+
+    private lateinit var mBinding: ContentMainBinding
+    private lateinit var mViewModel: RemindersViewModel
+    private lateinit var mReminderRecyclerAdapter: ReminderListAdapter
+    private lateinit var mAllReminders: List<Reminder>
+    private var mAppSettings: AppSettings? = null
+    private var mSpinner: Spinner? = null
+    private var mIsFetchError = false
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        mBinding = DataBindingUtil.inflate(
+                inflater, R.layout.content_main, container, false)
+        return mBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val myActivity = activity
+        if (myActivity != null) {
+            mViewModel = ViewModelProviders.of(myActivity).get(RemindersViewModel::class.java)
+            mBinding.lifecycleOwner = this
+            mBinding.viewmodel = mViewModel
+
+            getSettings()
+            displayApiFetchMessages()
+
+            //Setup the RecyclerView Adapter
+            mReminderRecyclerAdapter = ReminderListAdapter(context)
+            mReminderRecyclerAdapter.setOnItemClickListener(mViewModel)
+            val reminderRecyclerView = mBinding.reminderList
+            reminderRecyclerView.adapter = mReminderRecyclerAdapter
+            mReminderRecyclerAdapter.setDeleteReminderListener(this)
+
+            //Set the swipe to delete action
+            val itemTouchHelper = ItemTouchHelper(SwipeToDeleteCallback(mReminderRecyclerAdapter))
+            itemTouchHelper.attachToRecyclerView(reminderRecyclerView)
+        }
+    }
+
+    private fun getSettings() {
+            mViewModel.mSettings.observe(this, Observer { settings: AppSettings? ->
+                if (settings != null) {
+                    mAppSettings = settings
+                    setTheme()
+                    populateTheSpinner(settings.savedSpinnerPosition)
+
+                    if (settings.isFirstLaunch) {
+                        startActivity(Intent(activity, WelcomeActivity::class.java))
+                        return@Observer
+                    }
+                    if (!mIsFetchError)
+                        mViewModel.fetchAllAladhanData()
+
+                    if (settings.isShowOnBoardingTutorial) {
+                        showOnBoardingTutorial()
+                        settings.isShowOnBoardingTutorial = false
+                        mViewModel.updateSettings(settings)
+                    }
+                }
+            })
+        }
+
+    private fun showOnBoardingTutorial() {
+        TapTargetSequence(activity)
+                .targets(
+                        TapTarget.forView(activity?.findViewById(R.id.fab), getString(R.string.add_new_reminder), getString(R.string.add_new_reminder_description))
+                                .outerCircleColor(android.R.color.holo_blue_dark)
+                                .cancelable(false)
+                                .transparentTarget(true),
+                        TapTarget.forView(mSpinner, getString(R.string.spinner_tutorial), getString(R.string.spinner_tutorial_description))
+                                .outerCircleColor(android.R.color.holo_blue_dark)
+                                .cancelable(false)
+                                .transparentTarget(true),
+                        TapTarget.forToolbarOverflow(activity?.findViewById<View>(R.id.toolbar) as Toolbar,
+                                getString(R.string.change_theme),
+                                getString(R.string.change_theme_description))
+                                .outerCircleColor(android.R.color.holo_blue_dark)
+                                .transparentTarget(true)
+                                .cancelable(false)
+                                .tintTarget(true))
+                .listener(object : TapTargetSequence.Listener {
+                    override fun onSequenceFinish() {
+                        mReminderRecyclerAdapter.mShowOnBoardingTutorial = true
+                        mReminderRecyclerAdapter.notifyDataSetChanged()
+                    }
+                    override fun onSequenceStep(lastTarget: TapTarget, targetClicked: Boolean) {}
+                    override fun onSequenceCanceled(lastTarget: TapTarget) {}
+                })
+                .start()
+    }
+
+    private fun displayApiFetchMessages() {
+        mViewModel.errorMessages.observe(this, Observer { s: String? ->
+            val snackbar = Snackbar.make(mBinding.mainLayout, "", Snackbar.LENGTH_INDEFINITE)
+
+            if (!(s == null || s.isEmpty() || s.matches("Successful".toRegex()) || s.contains("Refreshing"))) {
+                //Error occurred
+                mIsFetchError = true
+
+                //Reset the month to 0 enabling refetching of the data when user selects refresh on the snackbar displayed
+                mAppSettings?.month = 0
+                mAppSettings?.let {  mViewModel.updateSettings(mAppSettings) }
+
+                snackbar.setText(s)
+                snackbar.setAction("Refresh") {
+                    mViewModel.fetchAllAladhanData()
+                    mIsFetchError = false
+                }
+                snackbar.show()
+            } else if (s != null && s.matches("Successful".toRegex()) && mAppSettings != null) //Successful
+                snackbar.dismiss() else if (s != null && s.contains("Refreshing")) //Ongoing
+                Toast.makeText(context, s, Toast.LENGTH_LONG).show()
+        })
+    }
+
+    private fun setTheme() {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) //Android 10 and above
+            return  //Follow Device Settings
+        if (mAppSettings?.isLightMode != false)
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        else
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES) //Dark Mode
+    }
+
+    /**
+     * Changes The Displayed data
+     *
+     * @param tag spinner Item
+     */
+    private fun changeDisplayedData(data: ArrayList<Reminder>?, tag: String) { //Checks to see if the data that changed affects what is being displayed by spinner selection
+//Returns early if it does not affect displayed data
+        if (!(mSpinner?.selectedItem as String).matches(tag.toRegex()))
+            return
+
+        val myActivity = activity
+        if (myActivity != null && data != null && data.isNotEmpty()) {
+            mAllReminders = data
+            if (mSpinner?.selectedItemPosition == 0 || mSpinner?.selectedItemPosition == 2) {
+                mBinding.nextReminder = null
+
+                for (reminder in data) {
+                    //Find the first reminder that is enabled and display it in the next reminder section
+                    if (reminder.isEnabled && reminder.timeInSeconds > TimeDateUtil.calculateOffsetFromMidnight()) {
+                        mBinding.nextReminder = reminder
+                        data.remove(reminder) //Remove The Next Reminder
+                        break
+                    }
+                }
+            }
+
+            mViewModel.mCategoriesToDisplay.observe(this, Observer { categoryToDisplay ->
+                myActivity.findViewById<View>(R.id.all_done_view)?.visibility = View.GONE
+
+                //Refresh the RecyclerView
+                if (!categoryToDisplay.matches("".toRegex())){
+                    val filteredData = data.filter { it.category.matches(categoryToDisplay.toRegex())}
+                    when {
+                        filteredData.isNotEmpty() -> {
+                            mReminderRecyclerAdapter.setData(filteredData)
+                        }
+                        else -> {
+                            mReminderRecyclerAdapter.setData(listOf())
+                            myActivity.findViewById<View>(R.id.all_done_view)?.visibility = View.VISIBLE
+                        }
+                    }
+                }
+                else {
+                    mReminderRecyclerAdapter.setData(data)
+                }
+            })
+
+            //Used to maintain category filter when user changes frequency
+            mViewModel.mCategoriesToDisplay.value = mViewModel.mCategoriesToDisplay.value
+        }
+        else  {
+            mBinding.nextReminder = null
+        }
+    }
+
+    private fun populateTheSpinner(savedSpinnerPosition: Int) {
+        val adapter = ArrayAdapter.createFromResource(context,
+                R.array.reminder_filter, android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        mSpinner = mBinding.spinner
+        mSpinner?.onItemSelectedListener = this
+        mSpinner?.adapter = adapter
+        mSpinner?.setSelection(savedSpinnerPosition)
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        //Save the spinner position which will be used when the app is launched again
+        mAppSettings?.savedSpinnerPosition = mSpinner?.selectedItemPosition ?: 0
+        mAppSettings?.let { mViewModel.updateSettings(mAppSettings) }
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View, position: Int, id: Long) {
+        mViewModel.getReminders(position)
+                .observe(this, Observer {
+                    reminders: List<Reminder>? ->
+                    changeDisplayedData(reminders as ArrayList<Reminder>?, mSpinner?.getItemAtPosition(position) as String) })
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {}
+
+    override fun deleteReminder(position: Int) {
+        val mDeletedReminder = mAllReminders[position]
+        if (mDeletedReminder.category.matches(SunnahAssistantUtil.PRAYER.toRegex())) {
+            val snackbar = Snackbar.make(mBinding.mainLayout, getString(R.string.cannot_delete_prayer_time),
+                    Snackbar.LENGTH_LONG)
+            snackbar.show()
+            mReminderRecyclerAdapter.notifyDataSetChanged()
+            return
+        }
+        if (mDeletedReminder.isEnabled)
+            mViewModel.cancelScheduledReminder(mDeletedReminder)
+
+        mViewModel.delete(mDeletedReminder)
+        val snackbar = Snackbar.make(mBinding.mainLayout, getString(R.string.delete_reminder),
+                Snackbar.LENGTH_LONG)
+        snackbar.setAction(getString(R.string.undo_delete)) { mViewModel.insert(mDeletedReminder) }
+        snackbar.show()
+    }
+}
