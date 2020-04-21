ebb17477d8073f7331cbe49da040f33907a45c15
Said Muhammad <saidmsaid81@gmail.com>
2020-04-22 00:07:30 +0300

    Fixed NPE when launching settings

--- app/src/main/java/com/thesunnahrevival/sunnahassistant/views/SettingsActivity.kt
+++ app/src/main/java/com/thesunnahrevival/sunnahassistant/views/SettingsActivity.kt
@@ -11,13 +11,14 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
         val transaction = supportFragmentManager.beginTransaction()
-        val fragment = if (intent.getStringExtra("fragmentToShow").matches("prayer".toRegex()))
+        val fragmentToShow = intent.getStringExtra("fragmentToShow") ?: "default"
+        val fragment = if (fragmentToShow.matches("prayer".toRegex()))
             SettingsFragment.newInstance(R.layout.prayer_time_settings)
         else
             SettingsFragment.newInstance(R.layout.settings_lists)
         transaction.replace(R.id.settings_container, fragment )
         transaction.commit()
-        supportActionBar?.setDisplayHomeAsUpEnabled(true);
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
     }
 
