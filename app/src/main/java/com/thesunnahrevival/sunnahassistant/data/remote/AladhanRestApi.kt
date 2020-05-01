5f233f2201cdc27c41e81e8bb8c7433961ca44e7
saidmsaid81 <saidmsaid81@gmail.com>
2020-05-01 22:10:23 +0300

    Fixed problems with coroutines

--- app/src/main/java/com/thesunnahrevival/sunnahassistant/data/remote/AladhanRestApi.kt
+++ app/src/main/java/com/thesunnahrevival/sunnahassistant/data/remote/AladhanRestApi.kt
@@ -9,6 +9,7 @@ import com.thesunnahrevival.sunnahassistant.utilities.TimeDateUtil
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -51,37 +52,39 @@ object AladhanRestApi  {
     }
 
     private fun processPrayerTimesData(prayerTimes: PrayerTimes?, reminderDAO :ReminderDAO) {
-        val prayerNames = arrayOf("Fajr Prayer", "Dhuhr Prayer", "Asr Prayer", "Maghrib Prayer", "Isha Prayer")
-        val listOfPrayerTimes = mutableListOf<Reminder>()
-        prayerTimes?.let {
-            val rawPrayerTimes = it.data
-            for (i in rawPrayerTimes.indices) {
-                val timings = rawPrayerTimes[i].timings
-                val day = rawPrayerTimes[i].date.gregorian.day
-                val fajrTime = timings.fajr.substring(0, 5)
-                val dhuhrTime = timings.dhuhr.substring(0, 5)
-                val asrTime = timings.asr.substring(0, 5)
-                val maghribTime = timings.maghrib.substring(0, 5)
-                val ishaTime = timings.isha.substring(0, 5)
-                val times = arrayOf(fajrTime, dhuhrTime, asrTime, maghribTime, ishaTime)
-                for (j in times.indices) {
-                    val reminder = Reminder(
-                            prayerNames[j],
-                            "",
-                            TimeDateUtil.getTimestampInSeconds(times[j]),
-                            SunnahAssistantUtil.PRAYER,
-                            SunnahAssistantUtil.DAILY,
-                            false, day,
-                            null, null, 0,
-                            null)
-                    listOfPrayerTimes.add(reminder)
+        CoroutineScope(Dispatchers.IO).launch {
+            val prayerNames = arrayOf("Fajr Prayer", "Dhuhr Prayer", "Asr Prayer", "Maghrib Prayer", "Isha Prayer")
+            val listOfPrayerTimes = mutableListOf<Reminder>()
+            prayerTimes?.let {
+                val rawPrayerTimes = it.data
+                for (i in rawPrayerTimes.indices) {
+                    val timings = rawPrayerTimes[i].timings
+                    val day = rawPrayerTimes[i].date.gregorian.day
+                    val fajrTime = timings.fajr.substring(0, 5)
+                    val dhuhrTime = timings.dhuhr.substring(0, 5)
+                    val asrTime = timings.asr.substring(0, 5)
+                    val maghribTime = timings.maghrib.substring(0, 5)
+                    val ishaTime = timings.isha.substring(0, 5)
+                    val times = arrayOf(fajrTime, dhuhrTime, asrTime, maghribTime, ishaTime)
+                    for (j in times.indices) {
+                        val reminder = Reminder(
+                                prayerNames[j],
+                                "",
+                                TimeDateUtil.getTimestampInSeconds(times[j]),
+                                SunnahAssistantUtil.PRAYER,
+                                SunnahAssistantUtil.DAILY,
+                                false, day,
+                                null, null, 0,
+                                null)
+                        listOfPrayerTimes.add(reminder)
+                    }
                 }
             }
-        }
 
-        CoroutineScope(Dispatchers.IO).launch {
             reminderDAO.addRemindersList(listOfPrayerTimes)
-            errorMessages.value = "Successful"
+            withContext(Dispatchers.Main){
+                errorMessages.value = "Successful"
+            }
         }
     }
 
