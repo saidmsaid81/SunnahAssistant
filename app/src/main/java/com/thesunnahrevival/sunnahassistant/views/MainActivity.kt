3292fcc2c3ef499f21a5f5775b32673c7ecf6a05
saidmsaid81 <saidmsaid81@gmail.com>
2020-08-19 01:21:21 +0300

    Added support for in app update

--- app/src/main/java/com/thesunnahrevival/sunnahassistant/views/MainActivity.kt
+++ app/src/main/java/com/thesunnahrevival/sunnahassistant/views/MainActivity.kt
@@ -7,6 +7,12 @@ import androidx.appcompat.app.AppCompatDelegate
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.play.core.appupdate.AppUpdateManager
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.InstallStatus
+import com.google.android.play.core.install.model.UpdateAvailability
 import com.google.android.play.core.review.ReviewManagerFactory
 import com.thesunnahrevival.sunnahassistant.R
 import com.thesunnahrevival.sunnahassistant.viewmodels.SunnahAssistantViewModel
@@ -16,6 +22,8 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity(){
+    private val requestCodeForUpdate: Int = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -40,7 +48,6 @@ class MainActivity : AppCompatActivity(){
             }
 
             withContext(Dispatchers.Main){
-                println(numberOfLaunches)
                 //Set theme
                 if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) //Android 9 and below
                     if (settings?.isLightMode != false)
@@ -51,8 +58,16 @@ class MainActivity : AppCompatActivity(){
                 if ((settings?.numberOfLaunches ?: 0) > 0 && (settings?.numberOfLaunches ?: 0) % 7 == 0){
                         showInAppReviewPrompt()
                 }
+                else if ((settings?.numberOfLaunches ?: 0) > 0 && (settings?.numberOfLaunches ?: 0) % 3 == 0){
+                    checkForUpdates()
+                }
             }
         }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 
     private fun showInAppReviewPrompt() {
@@ -67,8 +82,63 @@ class MainActivity : AppCompatActivity(){
         }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = this.findNavController(R.id.myNavHostFragment)
-        return navController.navigateUp()
+    private fun checkForUpdates() {
+        // Creates instance of the manager.
+        val appUpdateManager = AppUpdateManagerFactory.create(this)
+
+        // Returns an intent object that you use to check for an update.
+        val appUpdateInfoTask = appUpdateManager.appUpdateInfo
+
+        // Checks that the platform will allow the specified type of update.
+        appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                    && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE)
+            ) {
+
+                appUpdateManager.registerListener { state ->
+                    if (state.installStatus() == InstallStatus.DOWNLOADED) {
+                        // After the update is downloaded, show a notification
+                        // and request user confirmation to restart the app.
+                        popupSnackbarForCompleteUpdate(appUpdateManager)
+                    }
+                }
+                // Request the update.
+                appUpdateManager.startUpdateFlowForResult(
+                        appUpdateInfo,
+                        AppUpdateType.FLEXIBLE,
+                        this,
+                        requestCodeForUpdate)
+            }
+        }
     }
+
+    /* Displays the snackbar notification and call to action. */
+    private fun popupSnackbarForCompleteUpdate(appUpdateManager: AppUpdateManager) {
+        Snackbar.make(
+                findViewById(R.id.coordinator_layout),
+                getString(R.string.update_downloaded),
+                Snackbar.LENGTH_INDEFINITE
+        ).apply {
+            setAction(getString(R.string.restart)) { appUpdateManager.completeUpdate() }
+            show()
+        }
+    }
+
+    // Checks that the in app update is not stalled during 'onResume()'.
+    override fun onResume() {
+        super.onResume()
+
+        // Creates instance of the manager.
+        val appUpdateManager = AppUpdateManagerFactory.create(this)
+        appUpdateManager
+                .appUpdateInfo
+                .addOnSuccessListener { appUpdateInfo ->
+                    // If the update is downloaded but not installed,
+                    // notify the user to complete the update.
+                    if (appUpdateInfo.installStatus() == InstallStatus.DOWNLOADED) {
+                        popupSnackbarForCompleteUpdate(appUpdateManager)
+                    }
+                }
+    }
+
 }
