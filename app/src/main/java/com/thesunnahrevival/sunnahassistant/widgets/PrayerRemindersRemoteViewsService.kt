885734caf099e0ad03164d1621217c7eb94c07d7
saidmsaid81 <saidmsaid81@gmail.com>
2020-08-20 15:27:50 +0300

    Removed unnecessary parameter

--- app/src/main/java/com/thesunnahrevival/sunnahassistant/widgets/PrayerRemindersRemoteViewsService.kt
+++ app/src/main/java/com/thesunnahrevival/sunnahassistant/widgets/PrayerRemindersRemoteViewsService.kt
@@ -2,9 +2,8 @@ package com.thesunnahrevival.sunnahassistant.widgets
 
 import android.content.Intent
 import android.widget.RemoteViewsService
-import java.util.*
 
-class PrayerRemindersRemoteViewsService(val locale: Locale): RemoteViewsService() {
+class PrayerRemindersRemoteViewsService(): RemoteViewsService() {
     override fun onGetViewFactory(intent: Intent?): RemoteViewsFactory {
         return PrayerTimesRemoteViewsFactory(this.applicationContext, intent)
     }
