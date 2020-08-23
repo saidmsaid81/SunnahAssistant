38275936d2e26a022a423b22c54bb28f3744227c
saidmsaid81 <saidmsaid81@gmail.com>
2020-08-23 16:49:34 +0300

    Added a prompt to help in translating the app

--- app/src/main/java/com/thesunnahrevival/sunnahassistant/views/MainActivity.kt
+++ app/src/main/java/com/thesunnahrevival/sunnahassistant/views/MainActivity.kt
@@ -1,36 +1,38 @@
 package com.thesunnahrevival.sunnahassistant.views
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.google.android.material.snackbar.Snackbar
-import com.google.android.play.core.appupdate.AppUpdateManager
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory
-import com.google.android.play.core.install.model.AppUpdateType
 import com.google.android.play.core.install.model.InstallStatus
-import com.google.android.play.core.install.model.UpdateAvailability
-import com.google.android.play.core.review.ReviewManagerFactory
 import com.thesunnahrevival.sunnahassistant.R
+import com.thesunnahrevival.sunnahassistant.utilities.supportedLocales
 import com.thesunnahrevival.sunnahassistant.viewmodels.SunnahAssistantViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.util.*
+
 
 class MainActivity : AppCompatActivity(){
-    private val requestCodeForUpdate: Int = 1
+    private lateinit var activity: MainActivity
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
         val navController = this.findNavController(R.id.myNavHostFragment)
-        NavigationUI.setupActionBarWithNavController(this,navController)
-
+        NavigationUI.setupActionBarWithNavController(this, navController)
+        activity = this
         getSettings()
 
     }
@@ -56,10 +58,26 @@ class MainActivity : AppCompatActivity(){
                         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES) //Dark Mode
 
                 if ((settings?.numberOfLaunches ?: 0) > 0 && (settings?.numberOfLaunches ?: 0) % 7 == 0){
-                        showInAppReviewPrompt()
+                        showInAppReviewPrompt(activity)
                 }
                 else if ((settings?.numberOfLaunches ?: 0) > 0 && (settings?.numberOfLaunches ?: 0) % 3 == 0){
-                    checkForUpdates()
+                    checkForUpdates(activity)
+                }
+
+                else if ((settings?.numberOfLaunches ?: 0) == 5 && !supportedLocales.contains(Locale.getDefault().language)){
+                    val listener = View.OnClickListener {
+                        val browserIntent = Intent(Intent.ACTION_VIEW,
+                                Uri.parse("https://crwd.in/sunnah-assistant"))
+                        if (browserIntent.resolveActivity(packageManager) != null) {
+                            startActivity(browserIntent);
+                        }
+                    }
+
+                    popupSnackbar(activity,
+                            getString(R.string.help_translate_app, Locale.getDefault().displayLanguage),
+                            10000,
+                            getString(R.string.translate),
+                            listener)
                 }
             }
         }
@@ -70,64 +88,9 @@ class MainActivity : AppCompatActivity(){
         return navController.navigateUp()
     }
 
-    private fun showInAppReviewPrompt() {
-        val manager = ReviewManagerFactory.create(this)
-        val request = manager.requestReviewFlow()
-        request.addOnCompleteListener {
-            if (it.isSuccessful) {
-                // We got the ReviewInfo object
-                val reviewInfo = it.result
-                manager.launchReviewFlow(this, reviewInfo)
-            }
-        }
-    }
-
-    private fun checkForUpdates() {
-        // Creates instance of the manager.
-        val appUpdateManager = AppUpdateManagerFactory.create(this)
-
-        // Returns an intent object that you use to check for an update.
-        val appUpdateInfoTask = appUpdateManager.appUpdateInfo
-
-        // Checks that the platform will allow the specified type of update.
-        appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
-            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
-                    && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE)
-            ) {
-
-                appUpdateManager.registerListener { state ->
-                    if (state.installStatus() == InstallStatus.DOWNLOADED) {
-                        // After the update is downloaded, show a notification
-                        // and request user confirmation to restart the app.
-                        popupSnackbarForCompleteUpdate(appUpdateManager)
-                    }
-                }
-                // Request the update.
-                appUpdateManager.startUpdateFlowForResult(
-                        appUpdateInfo,
-                        AppUpdateType.FLEXIBLE,
-                        this,
-                        requestCodeForUpdate)
-            }
-        }
-    }
-
-    /* Displays the snackbar notification and call to action. */
-    private fun popupSnackbarForCompleteUpdate(appUpdateManager: AppUpdateManager) {
-        Snackbar.make(
-                findViewById(R.id.coordinator_layout),
-                getString(R.string.update_downloaded),
-                Snackbar.LENGTH_INDEFINITE
-        ).apply {
-            setAction(getString(R.string.restart)) { appUpdateManager.completeUpdate() }
-            show()
-        }
-    }
-
     // Checks that the in app update is not stalled during 'onResume()'.
     override fun onResume() {
         super.onResume()
-
         // Creates instance of the manager.
         val appUpdateManager = AppUpdateManagerFactory.create(this)
         appUpdateManager
@@ -136,9 +99,9 @@ class MainActivity : AppCompatActivity(){
                     // If the update is downloaded but not installed,
                     // notify the user to complete the update.
                     if (appUpdateInfo.installStatus() == InstallStatus.DOWNLOADED) {
-                        popupSnackbarForCompleteUpdate(appUpdateManager)
+                        popupSnackbar(activity, getString(R.string.update_downloaded),  Snackbar.LENGTH_INDEFINITE,
+                                getString(R.string.restart), View.OnClickListener { appUpdateManager.completeUpdate() } )
                     }
                 }
     }
-
 }
