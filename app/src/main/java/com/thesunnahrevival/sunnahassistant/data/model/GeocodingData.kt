9565a28b49e2f03e1e0e70b236512c966676634e
saidmsaid81 <saidmsaid81@gmail.com>
2020-12-19 01:01:13 +0300

    Properly handled geocoding api fetch errors

--- app/src/main/java/com/thesunnahrevival/sunnahassistant/data/model/GeocodingData.kt
+++ app/src/main/java/com/thesunnahrevival/sunnahassistant/data/model/GeocodingData.kt
@@ -3,7 +3,7 @@ package com.thesunnahrevival.sunnahassistant.data.model
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
-data class GeocodingData(val results: List<Result>)
+data class GeocodingData(val results: List<Result>, val status: String)
 
 data class Result(@field:Expose @field:SerializedName("formatted_address") val formattedAddress: String, val geometry: Geometry)
 
