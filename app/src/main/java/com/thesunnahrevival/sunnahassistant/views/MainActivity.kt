20d3543808ead994ccd6022a59c7f3cd136fecc6
Said Muhammad <saidmsaid81@gmail.com>
2022-04-24 23:50:40 +0300

    Replaced snackbars with banners for passing miscellaneous information

--- app/src/main/java/com/thesunnahrevival/sunnahassistant/views/MainActivity.kt
+++ app/src/main/java/com/thesunnahrevival/sunnahassistant/views/MainActivity.kt
@@ -2,6 +2,7 @@ package com.thesunnahrevival.sunnahassistant.views
 
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
@@ -65,6 +66,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun applySettings(settings: AppSettings) {
+        Log.v("Random Count", settings.numberOfLaunches.toString())
         //Set theme
         if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) //Android 9 and below
             if (settings.isLightMode)
@@ -72,18 +74,17 @@ class MainActivity : AppCompatActivity() {
             else
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES) //Dark Mode
 
-        if (settings.numberOfLaunches > 0 && settings.numberOfLaunches % 5 == 0) {
-            showInAppReviewPrompt(activity)
-        } else if (settings.numberOfLaunches > 0 && settings.numberOfLaunches % 3 == 0) {
+        if (settings.numberOfLaunches > 0 && settings.numberOfLaunches % 3 == 0) {
             checkForUpdates(activity)
-        } else if (settings.numberOfLaunches > 0 && settings.numberOfLaunches % 4 == 0) {
-            val random = Random.nextInt(1, 4)
-
+        } else if (settings.numberOfLaunches > 0 && settings.numberOfLaunches % 5 == 0) {
+            val random = Random.nextInt(1, 5)
+            Log.v("Random", random.toString())
             //Work-around to get the active fragment
-            val navHostFragment: Fragment? = supportFragmentManager.findFragmentById(R.id.myNavHostFragment)
+            val navHostFragment: Fragment? = supportFragmentManager
+                .findFragmentById(R.id.myNavHostFragment)
             val fragment = navHostFragment?.childFragmentManager?.fragments?.get(0)
 
-            if (fragment is MainFragment){
+            if (fragment is MainFragment) {
                 when (random) {
                     1 -> {
                         showHelpTranslateSnackBar(fragment)
@@ -91,8 +92,11 @@ class MainActivity : AppCompatActivity() {
                     2 -> {
                         showSendFeedbackSnackBar(fragment)
                     }
-                    else -> {
+                    3 -> {
                         showShareAppSnackBar(fragment)
+                    }
+                    4 -> {
+                        showInAppReviewPrompt(activity)
                     }
                 }
             }
@@ -112,14 +116,16 @@ class MainActivity : AppCompatActivity() {
         // Creates instance of the manager.
         val appUpdateManager = AppUpdateManagerFactory.create(this)
         appUpdateManager
-                .appUpdateInfo
-                .addOnSuccessListener { appUpdateInfo ->
-                    // If the update is downloaded but not installed,
-                    // notify the user to complete the update.
-                    if (appUpdateInfo.installStatus() == InstallStatus.DOWNLOADED) {
-                        popupSnackbar(activity, getString(R.string.update_downloaded), Snackbar.LENGTH_INDEFINITE,
-                                getString(R.string.restart)) { appUpdateManager.completeUpdate() }
-                    }
+            .appUpdateInfo
+            .addOnSuccessListener { appUpdateInfo ->
+                // If the update is downloaded but not installed,
+                // notify the user to complete the update.
+                if (appUpdateInfo.installStatus() == InstallStatus.DOWNLOADED) {
+                    popupSnackbar(
+                        activity, getString(R.string.update_downloaded), Snackbar.LENGTH_INDEFINITE,
+                        getString(R.string.restart)
+                    ) { appUpdateManager.completeUpdate() }
                 }
+            }
     }
 }
