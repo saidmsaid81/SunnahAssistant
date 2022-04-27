659eb8f71bee8aae5c5730ee95bd1f259784ae05
saidmsaid81 <saidmsaid81@gmail.com>
2022-04-27 16:24:46 +0300

    Fixed bug on hijri widget that allowed creating widget when no widget option selected. Now, when widget is clicked, it opens the app. Reduced font of next reminder in widget
    Signed-off-by: saidmsaid81 <saidmsaid81@gmail.com>

--- app/src/main/java/com/thesunnahrevival/sunnahassistant/widgets/HijriDateWidgetConfigureActivity.kt
+++ app/src/main/java/com/thesunnahrevival/sunnahassistant/widgets/HijriDateWidgetConfigureActivity.kt
@@ -1,11 +1,11 @@
 package com.thesunnahrevival.sunnahassistant.widgets
 
-import android.app.Activity
 import android.appwidget.AppWidgetManager
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.switchmaterial.SwitchMaterial
 import com.thesunnahrevival.sunnahassistant.R
 import kotlinx.coroutines.CoroutineScope
@@ -15,18 +15,19 @@ import kotlinx.coroutines.launch
 /**
  * The configuration screen for the [HijriDateWidget] AppWidget.
  */
-class HijriDateWidgetConfigureActivity : Activity(), View.OnClickListener {
+class HijriDateWidgetConfigureActivity : AppCompatActivity(), View.OnClickListener {
     private var appWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID
 
 
-    public override fun onCreate(icicle: Bundle?) {
-        super.onCreate(icicle)
+    public override fun onCreate(bundle: Bundle?) {
+        super.onCreate(bundle)
 
         // Set the result to CANCELED.  This will cause the widget host to cancel
         // out of the widget placement if the user presses the back button.
         setResult(RESULT_CANCELED)
 
         setContentView(R.layout.hijri_date_widget_configure)
+        setSupportActionBar(findViewById(R.id.toolbar))
         findViewById<View>(R.id.add_button).setOnClickListener(this)
 
         // Find the widget id from the intent.
@@ -34,7 +35,8 @@ class HijriDateWidgetConfigureActivity : Activity(), View.OnClickListener {
         val extras = intent.extras
         if (extras != null) {
             appWidgetId = extras.getInt(
-                    AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID)
+                AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID
+            )
         }
 
         // If this activity was started with an intent without an app widget ID, finish with an error.
@@ -50,23 +52,25 @@ class HijriDateWidgetConfigureActivity : Activity(), View.OnClickListener {
         // It is the responsibility of the configuration activity to update the app widget
         val appWidgetManager = AppWidgetManager.getInstance(context)
         val hijriChecked = findViewById<SwitchMaterial>(R.id.display_hijri_date_switch).isChecked
-        val nextReminderChecked = findViewById<SwitchMaterial>(R.id.display_next_reminder_switch).isChecked
-        if (hijriChecked || nextReminderChecked){
+        val nextReminderChecked =
+            findViewById<SwitchMaterial>(R.id.display_next_reminder_switch).isChecked
+        if (hijriChecked || nextReminderChecked) {
             findViewById<TextView>(R.id.error).visibility = View.GONE
             CoroutineScope(Dispatchers.IO).launch {
                 updateWidgetSettings(context, hijriChecked, nextReminderChecked)
                 val appWidgetIds = arrayOf(appWidgetId)
-                fetchDateFromDatabase(context, appWidgetManager, appWidgetIds )
+                fetchDateFromDatabase(context, appWidgetManager, appWidgetIds)
             }
+            // Make sure we pass back the original appWidgetId
+            val resultValue = Intent()
+            resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
+            setResult(RESULT_OK, resultValue)
+            finish()
         }
         else
             findViewById<TextView>(R.id.error).visibility = View.VISIBLE
 
-        // Make sure we pass back the original appWidgetId
-        val resultValue = Intent()
-        resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
-        setResult(RESULT_OK, resultValue)
-        finish()
+
     }
 
 }
