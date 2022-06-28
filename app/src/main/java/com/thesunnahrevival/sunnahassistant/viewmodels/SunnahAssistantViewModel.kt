0aa1e698b6e31e11ed8de9b25732fcbaea712402
saidmsaid81 <saidmsaid81@gmail.com>
2022-06-28 11:40:11 +0300

    Updated gradle, kotlin compileSdk versions
    Signed-off-by: saidmsaid81 <saidmsaid81@gmail.com>

--- app/src/main/java/com/thesunnahrevival/sunnahassistant/viewmodels/SunnahAssistantViewModel.kt
+++ app/src/main/java/com/thesunnahrevival/sunnahassistant/viewmodels/SunnahAssistantViewModel.kt
@@ -102,7 +102,7 @@ class SunnahAssistantViewModel(application: Application) : AndroidViewModel(appl
             val connectivityManager = getApplication<Application>().applicationContext
                     .getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
             return connectivityManager.activeNetworkInfo == null ||
-                    !connectivityManager.activeNetworkInfo.isConnected
+                    !connectivityManager.activeNetworkInfo!!.isConnected
         }
 
     fun getGeocodingData(address: String) {
