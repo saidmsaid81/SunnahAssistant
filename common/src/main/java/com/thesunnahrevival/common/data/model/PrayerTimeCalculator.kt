e88441f27c78a6f57a2b7421e0d639da82b642ae
saidmsaid81 <saidmsaid81@gmail.com>
2022-09-02 17:02:35 +0300

    Fix bug when saving automatic prayer times
    Signed-off-by: saidmsaid81 <saidmsaid81@gmail.com>

--- common/src/main/java/com/thesunnahrevival/common/data/model/PrayerTimeCalculator.kt
+++ common/src/main/java/com/thesunnahrevival/common/data/model/PrayerTimeCalculator.kt
@@ -12,17 +12,21 @@ import java.util.*
 
 
 class PrayerTimeCalculator(
-        private val latitude: Double, private val longitude: Double,
-        private val calculationMethod: CalculationMethod, private val asrCalculationMethod: Madhab,
-        private val latitudeAdjustmentMethod: Int, private val prayerNames: Array<String>, private val categoryName: String) {
+    private val latitude: Double,
+    private val longitude: Double,
+    private val calculationMethod: CalculationMethod,
+    private val asrCalculationMethod: Madhab,
+    private val latitudeAdjustmentMethod: Int,
+    private val prayerNames: Array<String>,
+    private val categoryName: String
+) {
 
-    fun getPrayerTimeReminders(): ArrayList<Reminder>
-    {
+    fun getPrayerTimeReminders(): ArrayList<Reminder> {
         val reminders = arrayListOf<Reminder>()
-        for (day in 1..lastDayOfMonth){
+        for (day in 1..lastDayOfMonth) {
             val prayerTimesStrings = prayerTimeStrings(day)
 
-            for ((index, prayerTimeString) in prayerTimesStrings.withIndex()){
+            for ((index, prayerTimeString) in prayerTimesStrings.withIndex()) {
                 val reminder = createReminder(index, day, prayerTimeString)
                 reminders.add(reminder)
             }
@@ -32,8 +36,10 @@ class PrayerTimeCalculator(
     }
 
     private fun prayerTimeStrings(day: Int): Array<String> {
-        val dateObject  = GregorianCalendar(getYear(System.currentTimeMillis()).toInt(),
-                getMonthNumber(System.currentTimeMillis()), day).time
+        val dateObject = GregorianCalendar(
+            getYear(System.currentTimeMillis()).toInt(),
+            getMonthNumber(System.currentTimeMillis()), day
+        ).time
         val coordinates = Coordinates(latitude, longitude)
         val date: DateComponents = DateComponents.from(dateObject)
         val params: CalculationParameters = calculationMethod.parameters
@@ -41,25 +47,26 @@ class PrayerTimeCalculator(
         params.highLatitudeRule = HighLatitudeRule.values()[latitudeAdjustmentMethod]
         val prayerTimes = PrayerTimes(coordinates, date, params)
         val formatter = SimpleDateFormat("HH:mm", Locale.ENGLISH)
-        return arrayOf(formatter.format(prayerTimes.fajr),
-                formatter.format(prayerTimes.dhuhr), formatter.format(prayerTimes.asr),
-                formatter.format(prayerTimes.maghrib), formatter.format(prayerTimes.isha))
+        return arrayOf(
+            formatter.format(prayerTimes.fajr),
+            formatter.format(prayerTimes.dhuhr), formatter.format(prayerTimes.asr),
+            formatter.format(prayerTimes.maghrib), formatter.format(prayerTimes.isha)
+        )
     }
 
-    private fun createReminder(index: Int, day :Int, prayerTimeString: String): Reminder {
-        val reminder = Reminder(
-                prayerNames[index],
-                "",
-                getTimestampInSeconds(prayerTimeString),
-                categoryName,
-                Frequency.Daily,
-                false,
-                day,
-                getMonthNumber(System.currentTimeMillis()),
-                getYear(System.currentTimeMillis()).toInt()
+    private fun createReminder(index: Int, day: Int, prayerTimeString: String): Reminder {
+
+        return Reminder(
+            prayerNames[index],
+            "",
+            getTimestampInSeconds(prayerTimeString),
+            categoryName,
+            Frequency.Daily,
+            false,
+            day,
+            getMonthNumber(System.currentTimeMillis()),
+            getYear(System.currentTimeMillis()).toInt(),
+            id = parseInt("-$day$index")
         )
-        val id = "-$day$index"
-        reminder.id = parseInt(id)
-        return reminder
     }
 }
