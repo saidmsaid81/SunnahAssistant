c26673e35f218722be5ab2bbe598f3b7027e67ad
Said <saidmsaid81@gmail.com>
2025-06-22 21:42:45 +0300

    feat(Quran-Reader): Add SurahListFragment Composable Previews

--- app/src/main/java/com/thesunnahrevival/sunnahassistant/views/resourcesScreens/quran_reader/SurahListFragment.kt
+++ app/src/main/java/com/thesunnahrevival/sunnahassistant/views/resourcesScreens/quran_reader/SurahListFragment.kt
@@ -1,5 +1,6 @@
 package com.thesunnahrevival.sunnahassistant.views.resourcesScreens.quran_reader
 
+import android.content.res.Configuration
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,13 +10,16 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.thesunnahrevival.sunnahassistant.data.model.Surah
 import com.thesunnahrevival.sunnahassistant.theme.SunnahAssistantTheme
 import com.thesunnahrevival.sunnahassistant.viewmodels.SurahListViewModel
 import com.thesunnahrevival.sunnahassistant.views.SunnahAssistantFragment
@@ -69,4 +73,82 @@ class SurahListFragment : SunnahAssistantFragment() {
             }
         }
     }
+
+    @Composable
+    @Preview(
+        uiMode = Configuration.UI_MODE_NIGHT_YES,
+        locale = "en"
+    )
+    fun SurahListPreviewDark() {
+        val previewSurahs = previewSurahs()
+
+
+        SunnahAssistantTheme {
+            Surface {
+                Column(modifier = Modifier.padding(top = 16.dp)) {
+                    LazyColumn {
+                        items(previewSurahs.size) { index ->
+                            SurahItem(previewSurahs[index], false) { }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @Composable
+    @Preview(
+        name = "Arabic Dark Mode",
+        uiMode = Configuration.UI_MODE_NIGHT_YES,
+        locale = "ar"
+    )
+    fun SurahListPreviewDarkArabic() {
+        val previewSurahs = previewSurahs()
+
+        SunnahAssistantTheme {
+            Surface {
+                Column(modifier = Modifier.padding(top = 16.dp)) {
+                    LazyColumn {
+                        items(previewSurahs.size) { index ->
+                            SurahItem(previewSurahs[index], true) { }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @Composable
+    @Preview
+    fun SurahListPreview() {
+        val previewSurahs = previewSurahs()
+
+        SunnahAssistantTheme {
+            Surface {
+                Column(modifier = Modifier.padding(top = 16.dp)) {
+                    LazyColumn {
+                        items(previewSurahs.size) { index ->
+                            SurahItem(previewSurahs[index], false) { }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @Composable
+    private fun previewSurahs() = listOf(
+        Surah(1, "سورة الفاتحة", "Suratul Fatiha", true, 7, 1),
+        Surah(2, "سورة البقرة", "Suratul Baqarah", false, 286, 2),
+        Surah(3, "سورة آل عمران", "Suratul Aal-Imran", false, 200, 50),
+        Surah(4, "سورة النساء", "Suratul Nisa", false, 176, 77),
+        Surah(5, "سورة المائدة", "Suratul Maidah", false, 120, 106),
+        Surah(6, "سورة الأنعام", "Suratul An'am", true, 165, 128),
+        Surah(7, "سورة الأعراف", "Suratul A'raf", true, 206, 151),
+        Surah(8, "سورة الأنفال", "Suratul Anfal", false, 75, 177),
+        Surah(9, "سورة التوبة", "Suratul Tawbah", false, 129, 187),
+        Surah(10, "سورة يونس", "Suratul Yunus", true, 109, 208),
+        Surah(11, "سورة هود", "Suratul Hud", true, 123, 221),
+        Surah(12, "سورة يوسف", "Suratul Yusuf", true, 111, 235)
+    )
 }
