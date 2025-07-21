2589542ac7c4e609ebf0d2798777b18a30d9f675
saidmsaid81 <saidmsaid81@gmail.com>
2025-07-21 23:56:31 +0300

    style: Apply rounded corners and update background for Ayah translations

--- app/src/main/java/com/thesunnahrevival/sunnahassistant/views/resourcesScreens/quran_reader/AyahTranslationBottomSheetContent.kt
+++ app/src/main/java/com/thesunnahrevival/sunnahassistant/views/resourcesScreens/quran_reader/AyahTranslationBottomSheetContent.kt
@@ -38,6 +38,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.LinkAnnotation
@@ -462,7 +463,8 @@ fun TranslationDropdown(
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .background(MaterialTheme.colors.surface)
+                    .clip(RoundedCornerShape(8.dp))
+                    .background(MaterialTheme.colors.onError)
             ) {
                 translations.forEach { translation ->
                     Row(
