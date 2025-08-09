1d019d1787b4e257a9573c39be7bb0a9a599bfe3
saidmsaid81 <saidmsaid81@gmail.com>
2025-08-09 15:41:07 +0300

    feat: Implement ArabicTextWithTranslation component
    Introduces a reusable Composable function, ArabicTextWithTranslation, to display Arabic text alongside its translations and related actions like sharing and bookmarking.
    
    This commit also includes the following changes:
    - Refactors the AdhkaarReaderFragment to utilize the new ArabicTextWithTranslation component for displaying Adhkaar items.
    - Adds a shimmer effect placeholder for the ArabicTextWithTranslation component.
    - Adds a "Reference" string resource.

--- app/src/main/java/com/thesunnahrevival/sunnahassistant/views/resourcesScreens/adhkaar/AdhkaarReaderFragment.kt
+++ app/src/main/java/com/thesunnahrevival/sunnahassistant/views/resourcesScreens/adhkaar/AdhkaarReaderFragment.kt
@@ -4,33 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.ViewCompositionStrategy
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
+import com.thesunnahrevival.sunnahassistant.R
 import com.thesunnahrevival.sunnahassistant.theme.SunnahAssistantTheme
 import com.thesunnahrevival.sunnahassistant.viewmodels.AdhkaarViewModel
 import com.thesunnahrevival.sunnahassistant.views.SunnahAssistantFragment
-import com.thesunnahrevival.sunnahassistant.views.resourcesScreens.quran_reader.FootnoteText
-import com.thesunnahrevival.sunnahassistant.views.utilities.ArabicTextUtils
-import com.thesunnahrevival.sunnahassistant.views.utilities.FontLoader
+import com.thesunnahrevival.sunnahassistant.views.utilities.ArabicTextWithTranslation
+import com.thesunnahrevival.sunnahassistant.views.utilities.ArabicTextWithTranslationShimmer
+import com.thesunnahrevival.sunnahassistant.views.utilities.TranslationText
 import com.valentinilk.shimmer.shimmer
 
 class AdhkaarReaderFragment : SunnahAssistantFragment() {
@@ -54,73 +49,13 @@ class AdhkaarReaderFragment : SunnahAssistantFragment() {
 
                 val pagerState = rememberPagerState(initialPage = chapterId) { 133 }
                 val uiState by viewModel.getAdhkaarItemsByChapterId(pagerState.currentPage).collectAsState()
-                val uthmaniFont = FontLoader.loadUthmaniFont()
-                val baseTextSize = 18.sp
-                val arabicTextScale = 1.4f
-
 
                 SunnahAssistantTheme {
                     Surface {
                         if (uiState.isLoading) {
                             LazyColumn(modifier = Modifier.padding(16.dp).shimmer()) {
                                 items(6) { index ->
-                                    if (index > 0) {
-                                        Divider(
-                                            color = Color.LightGray.copy(alpha = 0.4f),
-                                            thickness = 1.dp,
-                                            modifier = Modifier.padding(vertical = 16.dp)
-                                        )
-                                    }
-
-                                    Box(
-                                        modifier = Modifier
-                                            .fillMaxWidth(0.3f)
-                                            .height(20.dp)
-                                            .background(
-                                                color = Color.Gray.copy(alpha = 0.2f),
-                                                shape = MaterialTheme.shapes.small
-                                            )
-                                    )
-
-                                    Spacer(modifier = Modifier.height(12.dp))
-
-                                    repeat(3) {
-                                        Box(
-                                            modifier = Modifier
-                                                .fillMaxWidth()
-                                                .height(28.dp)
-                                                .background(
-                                                    color = Color.Gray.copy(alpha = 0.15f),
-                                                    shape = MaterialTheme.shapes.medium
-                                                )
-                                        )
-                                        Spacer(modifier = Modifier.height(8.dp))
-                                    }
-
-                                    Box(
-                                        modifier = Modifier
-                                            .fillMaxWidth(0.2f)
-                                            .height(16.dp)
-                                            .background(
-                                                color = Color.Gray.copy(alpha = 0.2f),
-                                                shape = MaterialTheme.shapes.small
-                                            )
-                                    )
-
-                                    Spacer(modifier = Modifier.height(8.dp))
-
-                                    repeat(2) {
-                                        Box(
-                                            modifier = Modifier
-                                                .fillMaxWidth(if (it == 0) 1f else 0.8f)
-                                                .height(18.dp)
-                                                .background(
-                                                    color = Color.Gray.copy(alpha = 0.15f),
-                                                    shape = MaterialTheme.shapes.small
-                                                )
-                                        )
-                                        Spacer(modifier = Modifier.height(6.dp))
-                                    }
+                                    ArabicTextWithTranslationShimmer(index)
                                 }
                             }
                         } else {
@@ -129,63 +64,29 @@ class AdhkaarReaderFragment : SunnahAssistantFragment() {
                                     items(uiState.adhkaarItems.size) { index ->
                                         val adhkaarItem = uiState.adhkaarItems[index]
 
-                                        if (index > 0) {
-                                            Divider(
-                                                color = Color.LightGray,
-                                                thickness = 1.dp,
-                                                modifier = Modifier.padding(vertical = 16.dp)
+                                        val translationTexts = if (adhkaarItem.englishText != null) {
+                                            listOf(
+                                                TranslationText(
+                                                    title = LocalConfiguration.current.locales[0].displayLanguage,
+                                                    text = adhkaarItem.englishText,
+                                                    footnoteLabel = stringResource(R.string.reference),
+                                                    footnotes = if (adhkaarItem.reference != null) listOf(adhkaarItem.reference) else listOf()
+                                                )
                                             )
+                                        } else {
+                                            listOf()
                                         }
 
-                                        Text(
-                                            text = "${adhkaarItem.itemId}",
-                                            style = MaterialTheme.typography.subtitle1,
-                                            fontWeight = FontWeight.Bold,
-                                            color = MaterialTheme.colors.primary
+                                        ArabicTextWithTranslation(
+                                            context = requireContext(),
+                                            index = index,
+                                            sectionMarker = adhkaarItem.itemId.toString(),
+                                            arabicText = adhkaarItem.arabicText ?: "",
+                                            translationTexts = translationTexts,
+                                            textToShare = "${adhkaarItem.arabicText}\n\n" +
+                                                    "${adhkaarItem.englishText}\n\n" +
+                                                    stringResource(R.string.reference) + ": ${adhkaarItem.reference}"
                                         )
-
-                                        Column(modifier = Modifier.fillMaxWidth()) {
-                                            adhkaarItem.arabicText?.let {
-                                                Text(
-                                                    text = ArabicTextUtils.formatArabicText(it),
-                                                    fontSize = baseTextSize * arabicTextScale,
-                                                    fontFamily = uthmaniFont,
-                                                    textAlign = TextAlign.Right,
-                                                    letterSpacing = 1.sp,
-                                                    lineHeight = (baseTextSize * arabicTextScale * 1.6f),
-                                                    modifier = Modifier
-                                                        .fillMaxWidth()
-                                                        .padding(bottom = 16.dp, start = 8.dp, end = 8.dp)
-                                                        .fillMaxWidth(),
-                                                    style = MaterialTheme.typography.body1.copy(
-                                                        fontFeatureSettings = "liga"
-                                                    )
-                                                )
-                                            }
-
-                                            adhkaarItem.englishText?.let {
-                                                Text(
-                                                    text = "English",
-                                                    fontSize = 14.sp,
-                                                    color = MaterialTheme.colors.onSurface.copy(alpha = 0.7f)
-                                                )
-
-                                                Column {
-                                                    Text(
-                                                        text = adhkaarItem.englishText,
-                                                        style = MaterialTheme.typography.body1.copy(
-                                                            fontSize = 16.sp,
-                                                            color = MaterialTheme.colors.onSurface
-                                                        ),
-                                                        modifier = Modifier.padding(bottom = 8.dp)
-                                                    )
-                                                }
-                                            }
-
-                                            adhkaarItem.reference?.let { FootnoteText(text = it) }
-
-                                            Spacer(modifier = Modifier.height(16.dp))
-                                        }
 
                                     }
                                 }
---
+++ app/src/main/java/com/thesunnahrevival/sunnahassistant/views/utilities/ArabicTextWithTranslation.kt
@@ -0,0 +1,210 @@
+package com.thesunnahrevival.sunnahassistant.views.utilities
+
+import android.content.Context
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.BookmarkAdd
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.thesunnahrevival.sunnahassistant.R
+import com.thesunnahrevival.sunnahassistant.views.resourcesScreens.quran_reader.BookmarkIcon
+import com.thesunnahrevival.sunnahassistant.views.resourcesScreens.quran_reader.CopyIcon
+import com.thesunnahrevival.sunnahassistant.views.resourcesScreens.quran_reader.ShareIcon
+
+@Composable
+fun ArabicTextWithTranslation(
+    context: Context,
+    index: Int,
+    sectionMarker: String,
+    arabicText: String,
+    translationTexts: List<TranslationText>,
+    textToShare: String
+) {
+
+    val uthmaniFont = FontLoader.loadUthmaniFont()
+    val baseTextSize = 18.sp
+    val arabicTextScale = 1.4f
+
+    if (index > 0) {
+        Divider(
+            color = Color.LightGray,
+            thickness = 1.dp,
+            modifier = Modifier.padding(vertical = 16.dp)
+        )
+    }
+
+    Text(
+        text = sectionMarker,
+        style = MaterialTheme.typography.subtitle1,
+        fontWeight = FontWeight.Bold,
+        color = MaterialTheme.colors.primary
+    )
+
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Text(
+            text = ArabicTextUtils.formatArabicText(arabicText),
+            fontSize = baseTextSize * arabicTextScale,
+            fontFamily = uthmaniFont,
+            textAlign = TextAlign.Right,
+            letterSpacing = 1.sp,
+            lineHeight = (baseTextSize * arabicTextScale * 1.6f),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp, start = 8.dp, end = 8.dp),
+            style = MaterialTheme.typography.body1.copy(
+                fontFeatureSettings = "liga"
+            )
+        )
+
+        for (translationText in translationTexts) {
+            Text(
+                text = translationText.title,
+                fontSize = 14.sp,
+                color = MaterialTheme.colors.onSurface.copy(alpha = 0.7f)
+            )
+
+            Text(
+                text = translationText.text,
+                style = MaterialTheme.typography.body1.copy(
+                    fontSize = 16.sp,
+                    color = MaterialTheme.colors.onSurface
+                ),
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+
+            if (translationText.footnotes.isNotEmpty()) {
+                Text(
+                    text = translationText.footnoteLabel,
+                    fontSize = 14.sp,
+                    color = MaterialTheme.colors.onSurface.copy(alpha = 0.7f)
+                )
+
+                for (footnote in translationText.footnotes) {
+                    Text(
+                        text = footnote,
+                        style = MaterialTheme.typography.caption,
+                        color = MaterialTheme.colors.onSurface,
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+
+                ShareIcon(
+                    context = context,
+                    textToShare = textToShare,
+                    modifier = Modifier.size(24.dp)
+                )
+
+                Spacer(modifier = Modifier.width(32.dp))
+
+                CopyIcon(
+                    context,
+                    textToShare,
+                    stringResource(R.string.copy_label),
+                    stringResource(R.string.copy_message),
+                    Modifier.size(24.dp)
+                )
+
+                Spacer(modifier = Modifier.width(32.dp))
+
+                BookmarkIcon(icon = Icons.Outlined.BookmarkAdd, modifier = Modifier.size(24.dp)) {
+//                                                    toggleAyahBookmark(selectedAyah.ayah)
+                }
+            }
+
+        }
+    }
+}
+
+data class TranslationText(val title: String, val text: String, val footnoteLabel: String, val footnotes: List<String>)
+
+@Composable
+fun ArabicTextWithTranslationShimmer(index: Int) {
+    Column(modifier = Modifier.fillMaxWidth().padding(horizontal = 8.dp)) {
+        if (index > 0) {
+            Spacer(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(1.dp)
+                    .background(Color.LightGray.copy(alpha = 0.5f))
+                    .padding(vertical = 16.dp)
+            )
+        }
+
+        Spacer(
+            modifier = Modifier
+                .width(80.dp)
+                .height(20.dp)
+                .padding(top = 8.dp)
+                .background(Color.LightGray.copy(alpha = 0.5f))
+        )
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(60.dp)
+                .background(Color.LightGray.copy(alpha = 0.5f))
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Spacer(
+            modifier = Modifier
+                .width(120.dp)
+                .height(16.dp)
+                .background(Color.LightGray.copy(alpha = 0.5f))
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(40.dp)
+                .background(Color.LightGray.copy(alpha = 0.5f))
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Spacer(
+                modifier = Modifier
+                    .size(24.dp)
+                    .background(Color.LightGray.copy(alpha = 0.5f))
+            )
+            Spacer(modifier = Modifier.width(32.dp))
+            Spacer(
+                modifier = Modifier
+                    .size(24.dp)
+                    .background(Color.LightGray.copy(alpha = 0.5f))
+            )
+            Spacer(modifier = Modifier.width(32.dp))
+            Spacer(
+                modifier = Modifier
+                    .size(24.dp)
+                    .background(Color.LightGray.copy(alpha = 0.5f))
+            )
+        }
+    }
+}
